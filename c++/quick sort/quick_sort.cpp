eaabe449b018635dd04b13c3d70323b1c2f83105
Dominik Wojciechowski <dominik.wojciechowski798@gmail.com>
2020-04-29 22:33:36 +0200

    quick sort: bug - algorithm works well with certain inputs only

--- c++/quick sort/quick_sort.cpp
+++ c++/quick sort/quick_sort.cpp
@@ -2,17 +2,28 @@
 #include <cstdlib>
 #include <ctime>
 
-#define N 20
+#define N 10
 
+
+void quickSort(int* list, int p, int r);
+int partition(int* list, int p, int r);
+void swap(int* a, int* b);
 int* generateList(int n);
 void printList(int* l, int n);
+
 
 int main(void) {
   srand(time(NULL));
 
   int* myList = generateList(N);
+  // int myList[] = {3, 7, 8, 2, 1};
   printList(myList, N);
 
+  std::cout << "sorting.." << std::endl;
+  quickSort(myList, 0, N);
+  printList(myList, N);
+
+  delete myList;
   return 0;
 }
 
@@ -31,4 +42,36 @@ void printList(int* l, int n) {
       std::cout << l[i] << ", ";
     }
     std::cout << l[i] << "]" << std::endl;;
+}
+
+void quickSort(int* list, int p, int r) {
+  if (p < r) {
+    int q = partition(list, p, r);
+    // std::cout << "q = " << q << std::endl;
+    quickSort(list, p, q - 1);
+    quickSort(list, q + 1, r);
+  }
+}
+
+int partition(int* list, int p, int r) {
+  r--;
+  int x = list[r];
+  int i = p - 1;
+  for (int j = p; j < r; j++) {
+    if (list[j] <= x) {
+      i++;
+      // std::cout << "swapping " << list[i] << " and " << list[j] << std::endl;
+      swap(&list[i], &list[j]);
+      // printList(list, r+1);
+    }
+  }
+  swap(&list[i + 1], &list[r]);
+  // std::cout << "returning" << std::endl;
+  return i + 1;
+}
+
+void swap(int* a, int* b) {
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
 }
